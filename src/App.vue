c6db6001d22a4a6bdfc660c83224324fd460eb81
russellpdesign <70049636+russellpdesign@users.noreply.github.com>
2026-05-08 11:19:22 -0700

    added Router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <Navbar />
+  <RouterView />
 </template>
